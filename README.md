92eb29b42fa4b82f75bfb294ab13f03261b299aa
felipesergios <felipe.sergio2011@gmail.com>
2021-01-13 13:38:10 -0300

    websocket

---
+++ README.md
@@ -0,0 +1,13 @@
+# Node Chat
+## plataforma básica para troca de mensagens
+Projeto base no [canal rocketseat](https://www.youtube.com/watch?v=-jXfKDYJJvo)
+1. Motivação
+    - Continuar o aprendizado da linguagem JS
+    - Aprender um pouco sobre websocket e real time
+2. Tecnologias e frameworks 
+    -NodeJS
+    -Html e Css
+    -Jquery
+    -Socket Io
+    -Express
+[Tela da plataforma](public/background.jpg)
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.set('views',path.join(__dirname,'public'))
+
+app.engine('html',require('ejs').renderFile)
+
+app.use('/',(req,res)=>{
+    res.render('index.html')
+})
+let messages = []
+io.on('connection',socket=>{
+    console.log(`socket conectado : ${socket.id}`)
+    socket.emit('previousMessages',messages)
+    socket.on('sendMessage', data =>{
+        console.log(data)
+        messages.push(data)
+        socket.broadcast.emit('receveivedMessage',data)
+    })
+})
+
+
+server.listen(3000)
